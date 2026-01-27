562ce425d12c97d1c04ddc44bc207008ad30812a
Szatocsvok <tobias.szabolcs.2021@osztalyterem.katolikuskeri.hu>
2026-01-27 22:20:19 +0100

    rendelesRoute.js alap megírása

--- Barbershop 1.0.6/Backend/src/controllers/rendelesekController.js
+++ Barbershop 1.0.6/Backend/src/controllers/rendelesekController.js
@@ -1,28 +1,83 @@
 /*
 rendelés készítés, módosítás, törlés
-rendelés időpontjának rögzítése
+rendelés időpontjának rögzítése !!!
 */
+const { sequelize } = require("../config/db.js");
+const Rendelesek = require("../models/rendelesekModel.js")(sequelize);
+const Termek = require("../models/termekModel.js")(sequelize);
 
+async function createRendeles({ vasarloNeve, vasarloEmail, telefonszam, iranyitoszam, telepules, szallitasiCim, termekek, ar }) {
+    const productsWithInfo = [];
 
-const { where } = require("sequelize");
-const { sequelize } = require("../../config/db.js");
-const rendeles = require("../models/rendelesekModel.js");
-const termekek = require("../models/termekModel.js")
+    for (const item of termekek) {
+        const product = await Termek.findByPk(item.termekId);
+        if (!product) throw new Error(`Termék ${item.termekId} nem található!`);
 
-async function itemAvaliableByName(termekNeve) {
-    try{
-        const termek = await termekek.findOne({ where: termekNeve })
-        return termek ? termek.toJSON() : null;
-    }catch(error){
-        console.error("Nem találjató ilyen névvel termék!", error);
-        throw error;
+        const snapshot = {
+            termekId: product.id,
+            nev: product.termekNev,
+            mennyiseg: item.mennyiseg,
+            ar: product.ar
+        };
+
+        totalPrice += product.ar * item.mennyiseg;
+        productsWithInfo.push(snapshot);
     }
+
+    const newRendeles = await Rendelesek.create({
+        vasarloNeve,
+        vasarloEmail,
+        telefonszam,
+        iranyitoszam,
+        telepules,
+        szallitasiCim,
+        rendelesIdeje: new Date(),
+        termekek: productsWithInfo,
+        ar
+    });
+
+    return newRendeles.toJSON();
 }
 
-async function itemAvaliableByName(params) {
-    
+async function modifyRendeles(id, data) {
+    const rendeles = await Rendelesek.findByPk(id);
+    if (!rendeles) {
+        throw new Error("Rendelés nem található!");
+    }
+
+    const updates = {};
+    if (data.vasarloNeve) updates.vasarloNeve = data.vasarloNeve;
+    if (data.vasarloEmail) updates.vasarloEmail = data.vasarloEmail;
+    if (data.telefonszam) updates.telefonszam = data.telefonszam;
+    if (data.iranyitoszam) updates.iranyitoszam = data.iranyitoszam;
+    if (data.telepules) updates.telepules = data.telepules;
+    if (data.szallitasiCim) updates.szallitasiCim = data.szallitasiCim;
+    if (data.termekek) updates.termekek = data.termekek;
+    if (data.ar !== undefined) updates.ar = data.ar;
+
+    const [updatedCount] = await Rendelesek.update(updates, { where: { id } });
+    if (updatedCount === 0) {
+        throw new Error("A rendelés módosítása sikertelen volt!");
+    }
+
+    const updatedRendeles = await Rendelesek.findByPk(id);
+    return updatedRendeles.toJSON();
+}
+
+async function deleteRendeles(id) {
+    const rendeles = await Rendelesek.findByPk(id);
+    if (!rendeles) {
+        throw new Error("Rendelés nem található!");
+    }
+
+    const rendelesDeleted = await rendeles.destroy();
+
+    return rendelesDeleted.toJSON();
 }
 
 module.exports = {
-    itemAvaliableByName
-}
+    createRendeles,
+    modifyRendeles,
+    deleteRendeles
+};
+
